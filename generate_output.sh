b77764004648f2c20e0fe0251efac034e381e90c
James Houghton <jamesthoughton@gmail.com>
2022-02-27 15:13:14 -0800

    Move output to its own directory

--- generate_output.sh
+++ generate_output.sh
@@ -2,7 +2,7 @@
 
 ARCHIVE=$1
 
-./check_languages.py --noarmory --difficulty easy $1 > easy_noarmory.txt
-./check_languages.py --noarmory --difficulty legendary $1 > legendary_noarmory.txt
-./check_languages.py --difficulty easy $1 > easy.txt
-./check_languages.py --difficulty legendary $1 > legendary.txt
+./check_languages.py --noarmory --difficulty easy $1 > output/easy_noarmory.txt
+./check_languages.py --noarmory --difficulty legendary $1 > output/legendary_noarmory.txt
+./check_languages.py --difficulty easy $1 > output/easy.txt
+./check_languages.py --difficulty legendary $1 > output/legendary.txt
