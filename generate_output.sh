3c837de7fb7227ccb81ec35a8ac3cd9764413c59
James Houghton <jamesthoughton@gmail.com>
2022-03-02 17:04:55 -0800

    Move breakdowns to own folder

--- generate_output.sh
+++ generate_output.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -eux
 
 ARCHIVE=${1:-sound-data.pkl}
+BREAKDOWNS=BREAKDOWNS
+mkdir -p output/$BREAKDOWNS
 
-./check_languages.py --stderrtotals --noarmory --difficulty easy $ARCHIVE 2> output/easy_noarmory.txt > output/easy_noarmory_breakdown.txt
-./check_languages.py --stderrtotals --noarmory --difficulty legendary $ARCHIVE 2> output/legendary_noarmory.txt > output/legendary_noarmory_breakdown.txt
-./check_languages.py --stderrtotals --difficulty easy $ARCHIVE 2> output/easy.txt > output/easy_breakdown.txt
-./check_languages.py --stderrtotals --difficulty legendary $ARCHIVE 2> output/legendary.txt > output/legendary_breakdown.txt
+./check_languages.py --stderrtotals --noarmory --difficulty easy $ARCHIVE 2> output/easy_noarmory.txt > output/$BREAKDOWNS/easy_noarmory.txt
+./check_languages.py --stderrtotals --noarmory --difficulty legendary $ARCHIVE 2> output/legendary_noarmory.txt > output/$BREAKDOWNS/legendary_noarmory.txt
+./check_languages.py --stderrtotals --difficulty easy $ARCHIVE 2> output/easy.txt > output/$BREAKDOWNS/easy.txt
+./check_languages.py --stderrtotals --difficulty legendary $ARCHIVE 2> output/legendary.txt > output/$BREAKDOWNS/legendary.txt
 
 function gen_with_excludes() {
     output=$1
     excludes="${@:2}"
     mkdir -p output/$output
-    ./check_languages.py --stderrtotals --noarmory --difficulty easy $ARCHIVE --exclude $excludes 2> output/$output/easy_noarmory.txt > output/$output/easy_noarmory_breakdown.txt
-    ./check_languages.py --stderrtotals --noarmory --difficulty legendary $ARCHIVE --exclude $excludes 2> output/$output/legendary_noarmory.txt > output/$output/legendary_noarmory_breakdown.txt
-    ./check_languages.py --stderrtotals --difficulty easy $ARCHIVE --exclude $excludes 2> output/$output/easy.txt > output/$output/easy_breakdown.txt
-    ./check_languages.py --stderrtotals --difficulty legendary $ARCHIVE --exclude $excludes 2> output/$output/legendary.txt > output/$output/legendary_breakdown.txt
+    mkdir -p output/$BREAKDOWNS/$output
+    ./check_languages.py --stderrtotals --noarmory --difficulty easy $ARCHIVE --exclude $excludes 2> output/$output/easy_noarmory.txt > output/$BREAKDOWNS/$output/easy_noarmory.txt
+    ./check_languages.py --stderrtotals --noarmory --difficulty legendary $ARCHIVE --exclude $excludes 2> output/$output/legendary_noarmory.txt > output/$BREAKDOWNS/$output/legendary_noarmory.txt
+    ./check_languages.py --stderrtotals --difficulty easy $ARCHIVE --exclude $excludes 2> output/$output/easy.txt > output/$BREAKDOWNS/$output/easy.txt
+    ./check_languages.py --stderrtotals --difficulty legendary $ARCHIVE --exclude $excludes 2> output/$output/legendary.txt > output/$BREAKDOWNS/$output/legendary.txt
 }
 
 gen_with_excludes cairo_hangar1_skip cairo_malta
